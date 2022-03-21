b797ea142c5a65fe06fd838a875290e0b3a801b8
naccham <early.summer6@gmail.com>
2022-03-21 14:31:43 +0000

    [Update]

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,6 +1,6 @@
 class Public::HomesController < ApplicationController
   def top
-    @thumbnail = "http://capture.heartrails.com/170x100/shorten?"
+    @thumbnail = "http://capture.heartrails.com/large?"
     @bookmark_ranks = Site.find(Bookmark.group(:site_id).order('count(site_id) desc').limit(3).pluck(:site_id))
   end
 end
--- app/controllers/public/sites_controller.rb
+++ app/controllers/public/sites_controller.rb
@@ -1,13 +1,13 @@
 class Public::SitesController < ApplicationController
 
   def index
-    @thumbnail = "http://capture.heartrails.com/170x100/shorten?"
-    @sites = Site.paginate(page: params[:page], per_page: 5)
+    @thumbnail = "http://capture.heartrails.com/large?"
+    @sites = Site.paginate(page: params[:page], per_page: 10)
     @bookmark_ranks = Site.find(Bookmark.group(:site_id).order('count(site_id) desc').limit(3).pluck(:site_id))
   end
-  
+
   def show
-    @thumbnail = "http://capture.heartrails.com/170x100/shorten?"
+    @thumbnail = "http://capture.heartrails.com/huge?"
     @site = Site.find(params[:id])
     @comment = Comment.new
   end
