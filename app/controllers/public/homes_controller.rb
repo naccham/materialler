4cc32f3594649cc93f8cc5772d81209c9d28149f
naccham <early.summer6@gmail.com>
2022-03-29 12:33:37 +0000

    update homes_controller

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -3,14 +3,12 @@ class Public::HomesController < ApplicationController
   def top
     @thumbnail = "http://capture.heartrails.com/huge?"
     @sites = Site.find(Bookmark.group(:site_id).order('count(site_id) desc').limit(10).pluck(:site_id))
-
     @tag1 = Tag.find(1)
     @tag2 = Tag.find(2)
     @tag3 = Tag.find(3)
     @tag4 = Tag.find(4)
     @tag5 = Tag.find(5)
     @tag6 = Tag.find(6)
-
   end
 
 end
